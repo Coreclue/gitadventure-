c7a9cfc78a6e94ec156acd553a889c3b558aa5c4
Coreclue <brian.mcintosh96@gmail.com>
2026-05-10 11:46:51 -0500

    add log loop count. add bubblesort2 function

--- bubblesort.js
+++ bubblesort.js
@@ -11,6 +11,34 @@ function bubbleSort(arr){
         }
     }
 
-bubbleSort([4, 5, 8, 20, 30])
+    console.log("total count:: " + count);
+    return arr;
+
+}
+
+function bubbleSort2(arr) {
+    let count = 0; 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length-1; j++) {
+            count++ 
+            if (arr[j] > arr[j+1]){
+                count++;
+                if(arr[j] > arr[j + 1]){
+                    let temp = arr[j+1];
+                    arr[j] = arr[j + 1];
+                    arr[j+1] = temp;
+
+                }
+
+            }
+        }
+    }
+    console.log("total count:", count)
+    return arr;
+}
+
+bubbleSort([4, 10, 8, 40, 30, 50, 60, 70, 80, 90, 100]);
+
+bubbleSort2([4, 10, 8, 40, 30, 50, 60, 70, 80, 90, 100]);
 
 
