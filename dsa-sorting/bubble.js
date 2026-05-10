252fd29f4a481bb542f80ad9b453e4d713a52ee3
Coreclue <brian.mcintosh96@gmail.com>
2026-05-10 14:21:53 -0500

    create bubbleSort function to return bubble sort algorithim

--- dsa-sorting/bubble.js
+++ dsa-sorting/bubble.js
@@ -1,3 +1,25 @@
-function bubbleSort() {}
+console.log("FILE IS RUNNING");
+//
+
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => { //swap values
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+// loop with i from end of array towards beginning.
+ // if arr[j] is greater than arr[j+1], swap two values.
+for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {  
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+// retrun assorted array.
+  return arr;
+}
+
+console.log(bubbleSort([2,11,9,26,31,35,60,70,700,7000,700000]));
+
+
 
 module.exports = bubbleSort;
