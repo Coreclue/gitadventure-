2a2b3c2b333966f8fa4601dafe10f265799c4c0b
Coreclue <brian.mcintosh96@gmail.com>
2026-05-11 13:37:16 -0500

    add selectionsort function.

--- dsa-sorting/selection.js
+++ dsa-sorting/selection.js
@@ -1,3 +1,18 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            [arr[i], arr[min]] = [arr[min], arr[i]]
+        }
+    }
+    return arr;
+}
 
 module.exports = selectionSort;
+
+console.log(selectionSort([4, 10, 8, 40, 30, 50, 60, 70, 80, 90, 100]));
