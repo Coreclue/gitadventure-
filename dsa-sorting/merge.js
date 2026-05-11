9be036fb1a1e357d0bf9f361b4816123b20fca70
Coreclue <brian.mcintosh96@gmail.com>
2026-05-11 11:22:40 -0500

    add comments, formats, for readability. while check check arrays push results. while loop iterates over 3 seperate condtions, continue iterating till all conditions are false.

--- dsa-sorting/merge.js
+++ dsa-sorting/merge.js
@@ -1,5 +1,35 @@
-function merge() {}
+// Merge Sort
+// merge arrays based on (n)log(n) time complexity and sorted input values. Sets conditions for j and i. push results with empty array to j and i.
+//
 
-function mergeSort() {}
+function merge(arr1, arr2) {
+    const results = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
 
-module.exports = { merge, mergeSort};
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+    return results;
+}
+
+const output = merge([1, 10, 50], [2, 12, 14, 99, 100]);
+console.log(output);
+
+function mergeSort() { }
+
+module.exports = { merge, mergeSort };
