5e00ce16ef79048b82f4c9186873e52cec7c86fa
Coreclue <brian.mcintosh96@gmail.com>
2026-05-22 12:36:32 -0500

    auto format.

--- Starter Code/spacecraft.js
+++ Starter Code/spacecraft.js
@@ -11,19 +11,18 @@ console.log(scanForLife());
 /* Task 3: Implicit Return with Objects: Log Coordinates */
 // TODO: Write an arrow function named `currentCoordinates` that returns an object with properties `x`, `y`, and `z`, representing coordinates in space. Use implicit return. Print the returned object from `currentCoordinates`.
 
-const currentCoordinates = () => ({x: 42, y: 87, z: 91});
+const currentCoordinates = () => ({ x: 42, y: 87, z: 91 });
 console.log(currentCoordinates());
 /* Task 4: Understanding `this`: Message from Home Base */
 // TODO: Inside an object named `spacecraft`, create a method named `receiveMessage` using arrow function syntax. This method should log `"Message received: "` followed by a message it receives as a parameter. Directly call `receiveMessage` within `spacecraft` and observe. Observe and explain the behavior of `this` in this context as a comment.
 
 const spacecraft = {
     name: " Galactic Voyager",
-    receivedMessage: (message) => 
-    {
+    receivedMessage: (message) => {
         console.log(`Message received: ${message}`);
         console.log(`This spacecraft is ${this.name}`);
     }
-} 
+}
 spaceecraft.receivedMessage("Hello from Planet Earth!")
 
 
