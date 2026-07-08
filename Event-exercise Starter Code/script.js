9cf6873b0fa7dc21b56603868f2f5e15691d117f
Coreclue <brian.mcintosh96@gmail.com>
2026-07-08 13:46:14 -0500

    load note from localStorage. load note ID counter from localStorage

--- Event-exercise Starter Code/script.js
+++ Event-exercise Starter Code/script.js
@@ -1,22 +1,29 @@
 // The script.js is included in the head of the index.html. To run your code after the DOM is loaded, wrap it with a callback function of an event listener (DOMContentLoaded)
 document.addEventListener("DOMContentLoaded", function () {
 
-    // 1 Get the elements from the DOM that you will work on (box container, new box button, color form, color input
-
-    const boxContainer = document.getElementById("box-container");
-    const newBoxButton = document.getElementById("new-box-button");
+    // 1 Get the elements from the DOM that you will work on (note container, new note button, color form, color input
+    const noteContainer = document.getElementById("note-container");
+    const newNoteButton = document.getElementById("new-note-button");
     const colorForm = document.getElementById("color-form");
     const colorInput = document.getElementById("color-input");
 
-    // 2 Create variables to store the box color and counter for the box ID.
 
-    let boxColor = null; // Stores the selected box color from the form.
-    let boxIdCounter = 0; // Counter for assigning unique IDs to new boxes.
+    // 2 Create variables to store the note color and counter for the note ID.
 
-    // 3 When the form is submitted, get the value from the color input element from Step 2 and set this color to all boxes (to get all boxes, use the class name box). Remember to reset the color input element's value and store the color in the box color variable we created in Step 3.
+    // TODO: Load the note color from the local storage.
+    function storage() {
+        let noteColor = localStorage.getItem("noteColor"); // Stores the selected note color from the form.
+        let noteIdCounter = 0; // Counter for assigning unique IDs to new notes.
+    }
 
-    colorForm.addEventListener("submit", function (event) {
-        event.preventDefault();
+
+
+    // TODO: Load the note ID counter from the local storage.
+    function loadIDCounter() {
+        noteIdCounter = parseInt(localStorage.getItem("noteIdCounter")) || 0;
+    }
+
+    loadIDCounter();
 
         boxColor = colorInput.value.trim();
 
