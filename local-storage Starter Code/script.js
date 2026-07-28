f045023b64c48e541d97690441cda4d14ca1a3f6
Coreclue <brian.mcintosh96@gmail.com>
2026-07-28 12:59:18 -0700

    no changes

--- local-storage Starter Code/script.js
+++ local-storage Starter Code/script.js
@@ -1,19 +1,39 @@
-document.addEventListener("DOMContentLoaded", function ()
-{
+document.addEventListener("DOMContentLoaded", function () {
+	// get references to the DOM elements that we will be working with.
 	const noteContainer = document.getElementById("note-container");
 	const newNoteButton = document.getElementById("new-note-button");
 	const colorForm = document.getElementById("color-form");
 	const colorInput = document.getElementById("color-input");
 
 	// TODO: Load the note color from the local storage.
-	let noteColor = null; // Stores the selected note color from the form.
-	// TODO: Load the note ID counter from the local storage.
+
+	let noteColor = localStorage.getItem("noteColor"); // Stores the selected note color from the form.
 	let noteIdCounter = 0; // Counter for assigning unique IDs to new notes.
+
+	// TODO: Load the note ID counter from the local storage.
+
+	function loadIDCounter() {
+		noteIdCounter = parseInt(localStorage.getItem("noteIdCounter")) || 0;
+	}
+
+	loadIDCounter();
+
 
 	// TODO: Load the notes from the local storage.
 
-	function addNewNote ()
-	{
+	function loadNotes() {
+		let notes = localStorage.getItem("notes");
+
+		if (!notes) {
+			notes = [];
+		} else {
+			notes = JSON.parse(notes);
+		}
+		return notes;
+	}
+
+
+	function addNewNote() {
 		const id = noteIdCounter;
 		const content = `Note ${id}`;
 
@@ -27,17 +47,40 @@ document.addEventListener("DOMContentLoaded", function ()
 		noteIdCounter++; // Increments the counter since the ID is used for this note.
 
 		// TODO: Add new note to the saved notes in the local storage.
+		let notes = displayNotes();
+		if (!notes) {
+			notes = [];
+		} else {
+			notes = JSON.parse(notes);
+		}
+		return notes;
+
+		notes.push({ id, content, color: noteColor });
+		localStorage.setItem("notes", JSON.stringify(notes));
+
 	}
 
-	colorForm.addEventListener("submit", function (event)
-	{
+	function displayNotes() {
+	for (const note of notes) {
+			notes = JSON.parse(notes);
+			const noteElement = document.createElement("textarea");
+			noteElement.setAttribute("data-note-id", note.id.toString()); // Stores the note ID to its data attribute.
+			noteElement.value = note.content; // Sets the note ID as value.
+			noteElement.className = "note"; // Sets a CSS class.
+			noteElement.style.backgroundColor = noteColor; // Sets the note's background color using the last selected note color.
+			noteContainer.appendChild(noteElement); // Appends it to the note container element as its child.
+		}
+	}
+
+
+
+	colorForm.addEventListener("submit", function (event) {
 		event.preventDefault(); // Prevents the default event.
 
 		const newColor = colorInput.value.trim();  // Removes whitespaces.
 
 		const notes = document.querySelectorAll(".note");
-		for (const note of notes)
-		{
+		for (const note of notes) {
 			note.style.backgroundColor = newColor;
 		}
 
@@ -45,44 +88,47 @@ document.addEventListener("DOMContentLoaded", function ()
 
 		noteColor = newColor; // Updates the stored note color with the new selection.
 
+		
 		// TODO: Update the note color in the local storage.
+
+		localStorage.setItem("noteColor", noteColor); // Saves the selected note color to the local storage.
+
 	});
 
-	newNoteButton.addEventListener("click", function ()
-	{
+	newNoteButton.addEventListener("click", function () {
 		addNewNote();
 	});
 
-	document.addEventListener("dblclick", function (event)
-	{
-		if (event.target.classList.contains("note"))
-		{
+	document.addEventListener("dblclick", function (event) {
+		if (event.target.classList.contains("note")) {
 			event.target.remove(); // Removes the clicked note.
 
 			// TODO: Delete the note from the saved notes in the local storage.
+			const noteId = event.target.dataset.noteId;
+			let notes = loadNotes();
+			// removes matching notes
+			notes = notes.filter(note => note.id.toString() !== noteId);
+			localStorage.setItem("notes", JSON.stringify(notes));
 		}
 	});
 
-	noteContainer.addEventListener("blur", function (event)
-	{
-		if (event.target.classList.contains("note"))
-		{
+	noteContainer.addEventListener("blur", function (event) {
+		if (event.target.classList.contains("note")) {
 			// TODO: Update the note from the saved notes in the local storage.
 		}
 	}, true);
 
-	window.addEventListener("keydown", function (event)
-	{
+	window.addEventListener("keydown", function (event) {
 		/* Ignores key presses made for color and note content inputs. */
-		if (event.target.id === "color-input" || event.target.type === "textarea")
-		{
-			return;
+		
+		if (event.target.id === "color-input" || event.target.type === "textarea") {
+			return; // Ignore the event if it is triggered from the color input element or note content input.
 		}
-
+		
 		/* Adds a new note when the "n" key is pressed. */
-		if (event.key === "n" || event.key === "N")
-		{
-			addNewNote(); // Adds a new note.
-		}
+		
+		if (event.key === "n" || event.key === "N") {
+				addNewNote(); // Adds a new note.
+			}
 	});
 });
