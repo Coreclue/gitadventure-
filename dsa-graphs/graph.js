f8b4831a8d10c392724935867d11efd78b4d5cbd
Coreclue <brian.mcintosh96@gmail.com>
2026-08-14 15:48:29 -0700

    add function to add instances to he graph properties. add function update update adjacant values.

--- dsa-graphs/graph.js
+++ dsa-graphs/graph.js
@@ -18,13 +18,22 @@ addVertex(vertex) {
 }
 
 // this function accepts an array of Node instances and adds them to the nodes property on the graph
-addVertices(vertexArray) { }
+addVertices(vertexArray) { 
+  let nodeArray = [];
+  for (array in nodeArray) {
+    this .nodeArray.add.Graph 
+}
 
 // this function accepts two vertices and updates their adjacent values to include the other vertex
-addEdge(v1, v2) { }
+addEdge(v1, v2) { 
+  v1.adjacent.add(v2);
+  v2.adjacent.add(v1);
+}
 
 // this function accepts two vertices and updates their adjacent values to remove the other vertex
-removeEdge(v1, v2) { }
+removeEdge(v1, v2) {
+  
+ }
 
 // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
 removeVertex(vertex) { }
@@ -47,4 +56,4 @@ graph.nodes.has(a) // true
 graph.nodes.has(b) // true
 graph.nodes.has(c) // true
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node } 
